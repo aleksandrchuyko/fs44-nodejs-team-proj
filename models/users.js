79d50c89852e4f6fbaf9e3b5d24183a8d33908ef
sergiihsv <hsv-serg@ukr.net>
2022-11-19 00:56:32 +0200

    start

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const Joi = require("joi");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
