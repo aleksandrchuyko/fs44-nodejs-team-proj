3bdac007ae004ed4cb45b7653dae2b9043093a36
sergiihsv <hsv-serg@ukr.net>
2022-11-19 04:49:26 +0200

    register done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("api/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
