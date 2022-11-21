07fcd4372e0ffd097e26948a1a280f5430ffaa44
sergiihsv <hsv-serg@ukr.net>
2022-11-21 19:23:08 +0200

    config swagger in app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const authRouter = require("./routes/api/auth");
 
@@ -14,7 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("/api/users", authRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
