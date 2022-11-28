47c28606a99ed076915ae57a965e3bce718daa11
sergiihsv <hsv-serg@ukr.net>
2022-11-28 18:23:56 +0200

    add session

--- models/session.js
+++ models/session.js
@@ -1,30 +1,5 @@
-/* const { Schema, model } = require("mongoose");
-
-const tokenSchema = new Schema(
-  {
-    refreshToken: {
-      type: String,
-      required: true,
-    },
-
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  },
-  { versionKey: false, timestamps: true }
-);
-
-const Token = model("token", tokenSchema);
-
-module.exports = { Token }; */
-
 const { Schema, model } = require("mongoose");
 
-/* const sessionSchema = new Schema({
-  uid: Schema.Types.ObjectId,
-});
- */
 const sessionSchema = new Schema({
   uid: Schema.Types.ObjectId,
 });
